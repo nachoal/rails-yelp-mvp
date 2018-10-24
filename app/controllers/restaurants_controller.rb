36e7d3a68ad969bfe174d5f356fb64d5a71bb9ae
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-24 13:13:40 -0500

    Finish V2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
+    @review = @restaurant.reviews.new
   end
 
   def new
